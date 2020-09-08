bfb3bf1527905c449603c07be3315a149acd1d1f
alexaspalato <alexaspalato@gmail.com>
2020-09-08 07:46:02 +0200

    apollo

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,15 @@
 const React = require("react")
 const GlobalContextProvider = require("./src/context/globalContextProvider")
   .GlobalContextProvider
+const GlobalApolloProvider = require("./src/apollo").GlobalApolloProvider
 
 require("typeface-bebas-neue")
 require("typeface-spartan")
 
 export const wrapRootElement = ({ element }) => {
-  return <GlobalContextProvider>{element}</GlobalContextProvider>
+  return (
+    <GlobalApolloProvider>
+      <GlobalContextProvider>{element}</GlobalContextProvider>
+    </GlobalApolloProvider>
+  )
 }
---
+++ src/apollo/index.js
@@ -0,0 +1,26 @@
+import config from "../../config"
+
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  createHttpLink,
+  ApolloLink,
+} from "@apollo/client"
+import fetch from "isomorphic-fetch"
+import React from "react"
+
+const httpLink = createHttpLink({
+  uri: `${config.wordPressUrl}graphql`,
+})
+
+const client = new ApolloClient({
+  link: ApolloLink.from([httpLink]),
+  cache: new InMemoryCache(),
+  fetch,
+})
+
+export const GlobalApolloProvider = ({ children }) => {
+  return <ApolloProvider client={client}>{children}</ApolloProvider>
+}

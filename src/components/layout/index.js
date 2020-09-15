764914d95cd1d11fb983f5f3b88c33a0d7b11be4
alexaspalato <alexaspalato@gmail.com>
2020-09-15 09:10:47 +0200

    fadeIn

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,13 +1,20 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui"
+import { useEffect } from "react"
 import PropTypes from "prop-types"
 import { Global } from "@emotion/core"
 import { globalStyles } from "../../styles"
 import { styles } from "./styles"
+import { gsap } from "gsap"
 
 export const Layout = ({ children }) => {
+  useEffect(() => {
+    setTimeout(() => {
+      gsap.to([".noFouc"], { duration: 1, opacity: 1 })
+    }, 100)
+  }, [])
   return (
-    <div sx={{ ...styles }}>
+    <div className="noFouc" sx={{ ...styles, opacity: 0 }}>
       <Global styles={globalStyles} />
       <div>
         <main>{children}</main>

9bc4d11e69bfc8cfc26b0f991f338d43210c6b57
alexaspalato <alexaspalato@gmail.com>
2020-09-15 09:46:37 +0200

    smallDRagons fade

--- src/components/smallDragons/index.js
+++ src/components/smallDragons/index.js
@@ -1,8 +1,9 @@
 /** @jsx jsx */
 import { jsx, Container, Flex } from "theme-ui"
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import { GlobalStateContext } from "../../context/globalContextProvider"
 import { Checkmark } from "grommet-icons"
+import { gsap } from "gsap"
 
 export const SmallDragons = ({ dragons }) => {
   const state = useContext(GlobalStateContext)
@@ -10,14 +11,23 @@ export const SmallDragons = ({ dragons }) => {
     .filter(item => item.detected)
     .map(item => item.title)
 
+  useEffect(() => {
+    setTimeout(() => {
+      gsap.to(".smallDragons", { duration: 2, opacity: 1 })
+      gsap.from(".smallDragons", { duration: 1, y: 10 })
+    }, 1000)
+  }, [])
+
   return (
     <div sx={{ bg: "green", py: 20 }}>
       <Container>
         <Flex
+          className="smallDragons"
           sx={{
             justifyContent: ["center", "space-between"],
             alignItems: "center",
             flexWrap: "wrap",
+            opacity: 0,
           }}
         >
           {dragons &&
@@ -36,7 +46,7 @@ export const SmallDragons = ({ dragons }) => {
               const done = state.currentQuestions > i
               const smallDragonImage = done ? darkDragon : whiteDragon
               return (
-                <div key={i} sx={{ position: "relative" }}>
+                <div className="dragon" key={i} sx={{ position: "relative" }}>
                   <div sx={{ px: 7 }}>
                     <img src={smallDragonImage} alt="" sx={{ m: 0 }} />
                     <div

54d2574d7bc12bf8dde02ac4ec117439ebe16344
Alexandra Spalato <alexaspalato@gmail.com>
2021-01-21 10:43:28 +0100

    config

--- config.js
+++ config.js
@@ -2,7 +2,8 @@
  * @type {{wordPressUrl: string}}
  */
 const config = {
-  wordPressUrl: `https://dragons-new.gatsby-wp.com/`,
+  // wordPressUrl: `https://dragons-new.gatsby-wp.com/`,
+  wordPressUrl: `http://localhost:10038/`,
 }
 
 module.exports = config

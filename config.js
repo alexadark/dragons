f4c3516cbff890f3740ec746afc6b0fe6dc19f6c
Alexandra Spalato <alexaspalato@gmail.com>
2021-01-23 08:26:12 +0100

    domain

--- config.js
+++ config.js
@@ -3,8 +3,7 @@
  */
 const config = {
   wordPressUrl: `https://dragons-new.gatsby-wp.com/`,
-  siteUrl: `https://dragons-app.netlify.app/`,
-  // wordPressUrl: `http://localhost:10038/`,
+  siteUrl: `https://knowyourdragons.com/`,
 }
 
 module.exports = config

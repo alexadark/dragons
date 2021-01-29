64282ccf0bc5d07c8246a0addf0e94d312f02154
Alexandra Spalato <alexaspalato@gmail.com>
2021-01-29 18:21:49 +0100

    optin text

--- src/components/submitForm/index.js
+++ src/components/submitForm/index.js
@@ -99,7 +99,7 @@ export const SubmitForm = ({ detectedDragonsData }) => {
       from: "Daniel G. Amen, MD <support@amenuniversity.com>",
       subject: "Your Results Are IN!",
       body: `
-      <em>*By opt-ing in, you are choosing to subscribe to our BrainMD and/or Amen Clinics email newsletters. You may opt out or change your preferences at any time. If you unsubscribe, you will still receive your results. Your results and information will never be distributed or shared. </em>
+
       <p style="font-family: 'Open Sans', sans-serif">
       Thank you for taking my Dragons from the Past Quiz. Based on your answers, the
       following Dragons have been detected:
@@ -239,6 +239,15 @@ export const SubmitForm = ({ detectedDragonsData }) => {
             <input type="submit" value="send my results" />
           </Flex>
         </form>
+        <Container sx={{ maxWidth: 500, textAlign: "center", fontSize: 14 }}>
+          <em>
+            *By opt-ing in, you are choosing to subscribe to our BrainMD and/or
+            Amen Clinics email newsletters. You may opt out or change your
+            preferences at any time. If you unsubscribe, you will still receive
+            your results. Your results and information will never be distributed
+            or shared.{" "}
+          </em>
+        </Container>
         {resultErrors && (
           <div
             sx={{
